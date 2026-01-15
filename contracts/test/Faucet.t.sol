e06c20c49bab99708c418fea87adc58ccbf5ad2f
sanmipaul <sanmipaul1@gmail.com>
2026-01-15 19:35:22 +0100

    Update MockERC20 to use USDC decimals

--- contracts/test/Faucet.t.sol
+++ contracts/test/Faucet.t.sol
@@ -6,8 +6,12 @@ import "../src/Faucet.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock cUSD", "mcUSD") {
-        _mint(msg.sender, 1000000 * 10**18);
+    constructor() ERC20("Mock USDC", "mUSDC") {
+        _mint(msg.sender, 1000000 * 10**6); // 6 decimals for USDC
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
     }
 }
 
